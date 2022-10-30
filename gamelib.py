68e1c192c4642914b10e54664b6a9ce7879845c6
Guilherme Lavezzo <guilherm909088@gmail.com>
2022-10-30 02:17:11 -0300

    Update port

--- gamelib.py
+++ gamelib.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
+
+
+port = int(os.environ.get('PORT', 5000))
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
